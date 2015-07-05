0975974b32d61dbce81846c08855fa80bae2c24d
Manuel HUEZ <manuel.huez@gmail.com>
2015-07-05 13:31:08 +0200

    Add processout to pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'processout',
+  packages = ['processout'],
+  version = '1.0.0',
+  description = 'ProcessOut API bindings for python',
+  author = 'Manuel Huez',
+  author_email = 'manuel@processout.com',
+  url = 'https://github.com/processout/processout-python',
+  download_url = 'https://github.com/processout/processout-python/tarball/1.0.0',
+  keywords = ['aggregation', 'payment', 'processor', 'API', 'bindings', 'ProcessOut'],
+  classifiers = [],
+)
