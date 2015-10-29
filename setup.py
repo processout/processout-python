5b263cd9c88e7e846fce3b38b9fbc069e809b13c
Manuel HUEZ <manuel.huez@gmail.com>
2015-10-29 17:55:25 +0100

    Update download lind and version number

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'processout',
   packages = ['ProcessOut'],
-  version = '2.1.0',
+  version = '2.1.1',
   description = 'ProcessOut API bindings.',
   author = 'ProcessOut',
   author_email = 'hi@processout.com',
   url = 'https://github.com/processout/processout-python',
-  download_url = 'https://github.com/processout/processout-python/tarball/2.0.0',
+  download_url = 'https://github.com/processout/processout-python/tarball/2.1.1',
   keywords = ['ProcessOut', 'api', 'bindings'],
   classifiers = [],
 )
