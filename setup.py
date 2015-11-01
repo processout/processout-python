40f8576f3b8892a2b37e36fd4c7a1cd38384aead
Manuel HUEZ <manuel.huez@gmail.com>
2015-11-01 19:10:53 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'processout',
   packages = ['ProcessOut'],
-  version = '2.1.1',
+  version = '2.1.2',
   description = 'ProcessOut API bindings.',
   author = 'ProcessOut',
   author_email = 'hi@processout.com',
   url = 'https://github.com/processout/processout-python',
-  download_url = 'https://github.com/processout/processout-python/tarball/2.1.1',
+  download_url = 'https://github.com/processout/processout-python/tarball/2.1.2',
   keywords = ['ProcessOut', 'api', 'bindings'],
   classifiers = [],
 )
